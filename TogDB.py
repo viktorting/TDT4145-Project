7d6d9b735ccc9b6ff5c84a827d34bddfc309b901
Tai <timonas@stud.ntnu.no>
2023-03-16 09:12:52 +0100

    Tasks added to correct files

--- TogDB.py
+++ TogDB.py
@@ -30,3 +30,27 @@ database.close()
 # cursor.execute('''INSERT INTO person VALUES (1, 'Ola Nordmann', '2002-02-02')''')
 # database.commit()
 # databse.close()
+
+
+""" 
+Oppgaver:
+
+c) For en stasjon som oppgis, skal bruker få ut alle togruter som er innom stasjonen en gitt ukedag. 
+
+
+d) Bruker skal kunne søke etter togruter som går mellom en startstasjon og en sluttstasjon, med
+utgangspunkt i en dato og et klokkeslett. Alle ruter den samme dagen og den neste skal
+returneres, sortert på tid.
+
+
+e) En bruker skal kunne registrere seg i kunderegisteret
+
+
+g) Registrerte kunder skal kunne finne ledige billetter for en oppgitt strekning på en ønsket togrute
+og kjøpe de billettene hen ønsker. (Husk å kun slege ledige plasser)
+
+
+h) For en bruker skal man kunne finne all informasjon om de kjøpene hen har gjort for fremtidige
+reiser
+
+"""
---
+++ TogDB.sql
@@ -0,0 +1,14 @@
+/* 
+
+a) Databasen skal kunne registrere data om alle jernbanestrekninger i Norge. Dere skal legge inn
+data for Nordlandsbanen (som vist i figuren).
+
+
+b) Dere skal kunne registrere data om togruter. Dere skal legge inn data for de tre togrutene på
+Nordlandsbanen som er beskrevet i vedlegget til denne oppgave. 
+
+
+f) Det skal legges inn nødvendige data slik at systemet kan håndtere billettkjøp for de tre togrutene
+på Nordlandsbanen, mandag 3. april og tirsdag 4. april i år.
+
+ */
